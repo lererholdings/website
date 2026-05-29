c3de223c071d4a6c7d9340d68759f794a85645ec
Yaakov (Coby) Lerer <coby_lerer@hotmail.com>
2026-05-29 21:49:08 +1000

    fix mis-route

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
-import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  
-  // Load indigo theme for /indigo route, otherwise globals loads default emerald
-  if (router.pathname === '/indigo') {
-    require('../styles/indigo.css');
-  }
-  
   return <Component {...pageProps} />;
 }
